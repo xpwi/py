83201d138130d248186a3de2eaf447ececf7c6df
xpwi <40421016+xpwi@users.noreply.github.com>
2018-08-20 20:09:25 +0800

    Add files via upload

---
+++ TensorFlow/tf05forward2.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+# 前向传播
+# 两层简单神经网络（全连接）
+# 向神经网络喂入1组特征
+import tensorflow as tf
+
+# 定义输入和参数
+# 用placeholder实现输入自定义（sess.run中喂一组数据）
+x = tf.placeholder(tf.float32, shape=(1, 2))
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
+
+# 定义前向传播的过程
+# 矩阵相乘，不运算
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+# 用会话计算结果
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    # 字典，喂入一组特征
+    print("y in tf05forward2 is:\n", sess.run(y, feed_dict={x:[[0.7,0.5]]}))
+
+# 结果：
+# [[3.0904665]]
